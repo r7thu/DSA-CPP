c713faae55a194f52fe2f4998de5117648d35517
Rithu <rithuu4@gmail.com>
2026-07-02 04:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (9.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         // 
-        // BRUTE FORCE
-        // 2553 ms runtime
-        // Normal Logic - Iterate for each element
+        // Better Solution
+        // Hashing Logic
         // 
         int n=nums.size();
-        // int maxCount=0;
-        // int maxElement;
+        map<int, int> mapp; // For Hash Map
         for(int i=0;i<n;i++){
-            int count=1;
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
-            if(count>n/2){
-                return nums[i];
+            mapp[nums[i]]++;
+        }
+
+        for(auto i : mapp){
+            if(i.second>n/2){
+                return i.first;
             }
         }
-        return 0;
+        return -1;
     }
 };
