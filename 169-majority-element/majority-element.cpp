61cc749a29d21acdc76acd5cdb39dacc0b0750f8
Rithu <rithuu4@gmail.com>
2026-07-02 03:49:12 +0530

    Time: 1157 ms (6.83%) | Memory: 28.3 MB (30.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -7,8 +7,8 @@ public:
         // Normal Logic - Iterate for each element
         // 
         int n=nums.size();
-        int maxCount=0;
-        int maxElement;
+        // int maxCount=0;
+        // int maxElement;
         for(int i=0;i<n;i++){
             int count=1;
             for(int j=i+1;j<n;j++){
@@ -16,11 +16,10 @@ public:
                     count++;
                 }
             }
-            if(count>maxCount){
-                maxCount=count;
-                maxElement=nums[i];
+            if(count>n/2){
+                return nums[i];
             }
         }
-        return maxElement;
+        return 0;
     }
 };
