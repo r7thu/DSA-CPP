44605b540e21e3a53c6715735d17d3bcda99159f
Rithu <rithuu4@gmail.com>
2026-07-02 05:10:11 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (30.96%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,11 +4,15 @@ public:
         // 
         // Optimal Solution
         // Moores Votting Algo
+        // Simple Idea Iterate through the loop 
+        // Adding count based on appearance of same element
+        // decreasing count based on appearance of other element
+        // if count become 0 start anew with new element
+        // Element at end of iteration is majority element 
         // 
         int n=nums.size();
         int count=0;
         int element;
-        // int in=1;
         for(int i=0;i<n;i++){
             if(count==0){
                 element=nums[i];
@@ -19,13 +23,17 @@ public:
                 }else
                     count--;
         }
-        int checkCount=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==element)
-                checkCount++;
-        }
-        if(checkCount>n/2)
-            return element;
-        return -1;
+
+        return element;
+
+        // If Majority Element not sure present then Check
+        // int checkCount=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==element)
+        //         checkCount++;
+        // }
+        // if(checkCount>n/2)
+        //     return element;
+        // return -1;
     }
 };
