3aa8e4b8b46d529e992f0a72d871ed1b9f07c70e
Rithu <rithuu4@gmail.com>
2026-07-01 21:23:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (9.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        //Idea is to make 3 pointers 
+        //compare m and n and update it to k
+        //m and n points to end of nums1 and nums2
+        //k points to actual end of nums1
         int k=m+n;
         while(n && m){
             if(nums2[n-1]>=nums1[m-1]){
@@ -15,6 +20,7 @@ public:
             }
         }
 
+        //we add this 2 while loops to add anything remaining
         while(n){
             nums1[k-1]=nums2[n-1];
             k--;
