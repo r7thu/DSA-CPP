cc83c500396240c402b9553809079dc9884b566a
Rithu <rithuu4@gmail.com>
2026-07-01 05:37:43 +0530

    Time: 7 ms (38.83%) | Memory: 23.8 MB (59.37%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // My method after watching yt
+        // Optimal Solution
         int n=intervals.size();
         sort(intervals.begin(),intervals.end()); //To Help Group 
         vector<vector<int>> arr;
-        arr.push_back(intervals.front()); //same as interval[0];
-        for(int i=1;i<n;i++){
-            if(intervals[i][0]<=arr.back()[1]){
-                arr.back()[1]=max(arr.back()[1],intervals[i][1]);
-            }
-            else
+
+        //Same Idea Executed Better
+        for(int i=0;i<n;i++){
+            //If Empty Or Doesnt Overlap then create new element in arr
+            if(arr.empty() || intervals[i][0]> arr.back()[1]){
                 arr.push_back(intervals[i]);
+            }
+            else{
+                //Means Overlapping Present so update arr.back()[1]
+                arr.back()[1]=max(intervals[i][1],arr.back()[1]);
+            }
         }
         return arr;
     }
