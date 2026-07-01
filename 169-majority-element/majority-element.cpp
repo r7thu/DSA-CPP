5bb80bc086a7f7c088a32191f383c6fa7b773b50
Rithu <rithuu4@gmail.com>
2026-07-02 04:54:26 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (9.25%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -8,9 +8,13 @@ public:
         int n=nums.size();
         map<int, int> mapp; // For Hash Map
         for(int i=0;i<n;i++){
-            mapp[nums[i]]++;
+            mapp[nums[i]]++; //Value store how much time the element appeared
         }
 
+        //method to Iterate a MAP
+        //Use Range Based Iteration
+        //as elements in Map are not index based they are based on red black trees
+        //Remenber to use auto always in range based iteration
         for(auto i : mapp){
             if(i.second>n/2){
                 return i.first;
