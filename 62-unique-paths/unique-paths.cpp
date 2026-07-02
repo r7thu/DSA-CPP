fb50cc32c53135e01c49a220aed7d86181ebf9a1
Rithu <rithuu4@gmail.com>
2026-07-03 03:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (29.64%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
-    // Memotization Approach
-    // Used 2D array called DP to store values
-    // So that it can reduce time complexity
-    // Important things we started to find from end to start Position
-    //
-    int solve(int m, int n , vector<vector<int>>& DP){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        if(DP[m][n]!=-1) return DP[m][n]; //Checking if present in DP
-        return DP[m][n]=solve(m-1,n,DP)+solve(m,n-1,DP); //Storing in DP
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> DP (m,vector<int> (n,-1)); //Important 2D Vector Initialization
-        int solution =solve(m-1,n-1,DP);
-        return solution;
+        // Changing into Tabular method
+        // Step 1 intialize DP array at start with same Size as in memotization
+        // Intialize Base Cases
+        // here only ar[0][0] is base case 
+        // so initialize in for loop 
+        // think and do rest
+        // Main clue is that same formula will be used here also
+
+        vector<vector<int>> DP (m,vector<int> (n,0));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    DP[0][0]=1;
+                }
+                else{
+                    int up=0,left=0;
+                    if(i>0)up=DP[i-1][j];   //We do this here because of First Row and Col
+                    if(j>0)left=DP[i][j-1]; //1st row and col have seperate case where it will be 0 and not change
+                    DP[i][j]=up+left;
+                }
+            }
+        }
+        return DP[m-1][n-1];
     }
 };
+
+
+
