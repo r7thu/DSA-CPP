d5dab023b476c8711a43215965717de36455433c
Rithu <rithuu4@gmail.com>
2026-07-03 01:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.79%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -9,11 +9,15 @@ public:
         int n=matrix.size();//row size
         int m=matrix[0].size();//col size
         int l=0;
-        int r=(n*m)-1;
+        int r=(n*m)-1; // 2D array Last index
         while(l<=r){
-            int mid=(l+r)/2;
-            int row=mid/m;
-            int col=mid%m;
+            // int mid=(l+r)/2;
+            int mid=l+(r-l)/2; 
+            //we do this to avoid Overflow if l and r is very big
+            // (l=r)/2 and l+(r-l)/2 are both same
+
+            int row=mid/m; //Important Logic to find row and col
+            int col=mid%m; //imp
             if(matrix[row][col]==target){
                 return true;
             }
