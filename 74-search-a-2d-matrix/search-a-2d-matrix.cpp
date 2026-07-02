d29d76273a9495344bc31e10efbd497d30ed39c3
Rithu <rithuu4@gmail.com>
2026-07-02 06:47:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (78.88%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,7 +3,10 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         //Brute Force Approach is Linear search through all elements
         // Which will lead to O(M*N);
-        // We are DOing Better Solution Now
+        // We are Doing Better Solution Now
+        //Approach is to start from upper right most element
+        //move down if target is greater
+        //move left if target is maller
         int m=matrix.size();
         int n=matrix[0].size();
         int i=0;
