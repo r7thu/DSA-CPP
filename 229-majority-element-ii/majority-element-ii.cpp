a496392fe5770d4db8a954bc3cd52777d1b311c0
Rithu <rithuu4@gmail.com>
2026-07-02 06:16:10 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (44.25%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        // Better Solution 
-        // Used Hashing Same as Majority 1
-        map<int,int> mapp;
-        vector <int> arr;
+        // Optimal Solution 
+        // Used Moores votting ALgo Same as Majority 1
+        // for each case made it into 2
+        // created edge case for if count==0 cases
         int n=nums.size();
-        int count=floor(n/3);
+        int count1=0;
+        int count2=0;
+        int el1=INT_MIN;
+        int el2=INT_MIN;
         for(int i=0;i<n;i++){
-            mapp[nums[i]]++;
-        }
-
-        for(auto i:mapp){
-            if(i.second>count){
-                arr.push_back(i.first);
+            if(count1==0 && el2!=nums[i]){
+                count1=1;
+                el1=nums[i];
             }
+            else if(count2==0 && el1!=nums[i]){
+                count2=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i])
+            count1++;
+            else if(el2==nums[i])
+            count2++;
+            else
+                count1--,count2--;
         }
+        vector <int> arr;
+        count1=0;
+        count2=0;
+        for(int i=0;i<n;i++){
+            if(el1==nums[i])count1++;
+            if(el2==nums[i])count2++;
+        }
+        if(count1>floor(n/3))
+            arr.push_back(el1);
+        if(count2>floor(n/3))
+            arr.push_back(el2);
         return arr;
     }
 };
