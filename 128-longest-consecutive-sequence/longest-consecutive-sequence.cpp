6515c05dffafd6f4a8a5bce4dd2dca2c33e227e5
Rithu <rithuu4@gmail.com>
2026-07-04 01:59:15 +0530

    Time: 20 ms (82.40%) | Memory: 60.1 MB (92.72%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     // 
-    // Solution 1 better approach O(nlogn)
-    // logn from sorting and n from loop
-    // Using Sorting and therefore grouping consecutive elements
-    // while iterate through the array once 
-    // checking if any 2 element pairs are consecutive
-    // if its consecutive then increase count and then check for maxCount
-    // if not then check if its dupleicate nums[i]==nums[i+1]
-    // if duplicate skip
-    // if not in any case above then start count oncemore from 1(start fresh)
-    // at end maxCount will have answer
+    // Solution 2 Optimal approach O(N)
+    // Without using Sorting 
+    // Using Unordered set to remove duplicates
+    // while iterating through the set only start entering if  
+    // the current element has no consecutive element before it (st[x-1] not present for x)
+    // this above step will help us avoid duplicates
+    // then in a while loop check if st[x+1]present for x
+    // if present increase count and x
+    // outside while loop find maxCount
+    // maxCount is the answer at the end
     // 
+    // SIMPLE IDEA
+    // we find starting points find how much it will go
+    // get the best count from those
+    // to remove duplicates we use unordered set
+
     int longestConsecutive(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         if(nums.empty())
