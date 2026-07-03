40c86da0e2f8fc3d8dd5f515d89718d89224079d
Rithu <rithuu4@gmail.com>
2026-07-03 20:08:45 +0530

    Time: 3 ms (66.88%) | Memory: 15.1 MB (9.74%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -11,20 +11,13 @@ public:
 
         int n=nums.size();
         map<int,int> mapp;
+        
         for(int i=0;i<n;i++){
-            // Stored index as Value of Key
-            // So that we can check for element not repeating
-            mapp[nums[i]] = i; 
-        }
-        for(int i=0;i<n;i++){
-            
             int complement = target - nums[i];
             if (mapp.find(complement) != mapp.end()) {
-                auto index= mapp.find(complement);
-                if(i==index->second)
-                    continue;
                 return {mapp[complement], i};
             }
+            mapp[nums[i]]=i;
         }
         return {-1,-1};
     }
