9403d759971f08fe32300c6d2e0844b1a576cc0e
Rithu <rithuu4@gmail.com>
2026-07-03 07:36:56 +0530

    Time: 35 ms (34.87%) | Memory: 13.9 MB (99.80%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //Brute Force Approach
+        // O(n^2)
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
