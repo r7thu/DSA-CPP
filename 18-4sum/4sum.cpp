a1604629585c640c98a7fd566a0514e6816bd4e6
Rithu <rithuu4@gmail.com>
2026-07-05 03:52:09 +0530

    Time: 1789 ms (5.02%) | Memory: 474.6 MB (5.00%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        // This is the Better Approach 
+        // More Confusing than 2 Sum Hash Approach
+        // we try to find the complement-> (target-n[i]+n[j]+n[k])
+        // if complement present then it will be the n[l] 
+        // Main changes are [ij---k---] we are checking with k to find n[l]
+        // We add nums[k] in hash set and check if the sum==target
+        // 
         int n=nums.size();
         set <vector<int>> st;
 
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 unordered_set <long long> hash;
-                // int value=nums[i]+nums[j];
                 for(int k=j+1;k<n;k++){
                     long long complement = (long long)target - nums[i] - nums[j] - nums[k];
-                    // int value1=value+nums[k];
-                    // int complement = target-value1;
                     if(hash.count(complement)){
                         vector<int> temp ={nums[i],nums[j],nums[k],(int)complement};
                         sort(temp.begin(),temp.end());
