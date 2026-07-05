d4b36b02300e981838c7221781c84b97e9b4d5c7
Rithu <rithuu4@gmail.com>
2026-07-06 05:24:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.87%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,10 +1,43 @@
 class Solution {
 public:
-    // Brute Force Solution
-    // Using Sorting Merge Sort 
-    // Time Complexity O(nLogn)
-    // Space Complexity O(N)
+    // Better Solution
+    // Without INPLACE
+    // Use an extra array to store element 
+    // Very easy idea
+    // Time Complexity O(N)
+    // Space Complexity O(1)
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        vector<int> result;
+        int zCount=0;
+        int oCount=0;
+        int tCount=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                zCount++;
+                continue;
+            }
+            if(nums[i]==1){
+                oCount++;
+                continue;
+            }
+            if(nums[i]==2){
+                tCount++;
+                continue;
+            }    
+        }
+
+        int index=0;
+        for(int i=0;i<zCount;i++){
+            nums[index]=0;
+            index++;
+        }
+        for(int i=0;i<oCount;i++){
+            nums[index]=1;
+            index++;
+        }
+        for(int i=0;i<tCount;i++){
+            nums[index]=2;
+            index++;
+        }
     }
 };
