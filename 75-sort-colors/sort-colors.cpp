b714c44862abc113350da03119afe7158324be1e
Rithu <rithuu4@gmail.com>
2026-07-06 05:43:32 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.81%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
-    // Better Solution
-    // Without INPLACE
-    // Use an extra array to store element 
-    // Very easy idea
+    // Optimal Solution
+    // With INPLACE
+    // Keep 3 Pointers
+    // 0--------low-1 for zero
+    // low------mid-1 for one
+    // mid------high-1 UNSORTED we move through this and sort
+    // high------n-1 for one
+    // So we start from mid to high and check for each element 
+    // Sorting it by 3 conditions each for 1,2,3
+    // reading the conditions will help u understand the rest
+
     // Time Complexity O(N)
     // Space Complexity O(1)
     void sortColors(vector<int>& nums) {
