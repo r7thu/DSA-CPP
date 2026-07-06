3ef52c499f7b26ad5b2d47e2d4a176d7f018bbaa
Rithu <rithuu4@gmail.com>
2026-07-06 08:40:59 +0530

    Time: 149 ms (5.51%) | Memory: 89.5 MB (8.17%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -5,11 +5,13 @@ public:
         map<int,int> mpp;
         for(int i=0;i<n;i++){
             mpp[nums[i]]++;
+            if(mpp[nums[i]]>1)
+                return nums[i];
         }
-        for(auto i:mpp){
-            if(i.second>1)
-                return i.first;
-        }
+        // for(auto i:mpp){
+        //     if(i.second>1)
+        //         return i.first;
+        // }
 
         return -1;
     }
