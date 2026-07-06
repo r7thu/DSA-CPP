7ea2302419959133573c1e7ac478cf2ddc731ba7
Rithu <rithuu4@gmail.com>
2026-07-06 05:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.87%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -7,37 +7,23 @@ public:
     // Time Complexity O(N)
     // Space Complexity O(1)
     void sortColors(vector<int>& nums) {
-        vector<int> result;
-        int zCount=0;
-        int oCount=0;
-        int tCount=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                zCount++;
-                continue;
+        int n=nums.size();
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
             }
-            if(nums[i]==1){
-                oCount++;
-                continue;
+            else if(nums[mid]==1){
+                mid++;
             }
-            if(nums[i]==2){
-                tCount++;
-                continue;
-            }    
-        }
-
-        int index=0;
-        for(int i=0;i<zCount;i++){
-            nums[index]=0;
-            index++;
-        }
-        for(int i=0;i<oCount;i++){
-            nums[index]=1;
-            index++;
-        }
-        for(int i=0;i<tCount;i++){
-            nums[index]=2;
-            index++;
+            else if(nums[mid]==2){
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
     }
 };
