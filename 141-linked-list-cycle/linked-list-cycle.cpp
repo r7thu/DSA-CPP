9f511f8357a9361d863b70a50cc4f8a6665dc78e
Rithu <rithuu4@gmail.com>
2026-07-09 07:48:47 +0530

    Time: 8 ms (83.24%) | Memory: 11.8 MB (53.89%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,20 +1,18 @@
 
 class Solution {
 public:
-    // Brute Force Solution
-    // Use a Hash Map to see if this Node is already visited
-    // If visited then return true
-    //TC O(N), SC O(N)
+  
     bool hasCycle(ListNode *head) {
-    unordered_map <ListNode* ,int> mpp; 
-    // This is how you make NODE as a key in Hash Map
-    ListNode* temp=head;
-    while(temp){
-        mpp[temp]++; 
-        if(mpp[temp]>1)
+    ListNode* slow=head;  
+    ListNode* fast=head; 
+     
+    while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(fast==slow){
             return true;
-        temp=temp->next;
-    } 
-    return false;   
+        }
+    }
+    return false;
     }
 };
