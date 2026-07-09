ff02792d5e99f5218e4b3c6181ce0cd48797e9ad
Rithu <rithuu4@gmail.com>
2026-07-09 07:52:29 +0530

    Time: 13 ms (20.26%) | Memory: 11.8 MB (53.89%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,7 +1,8 @@
 
 class Solution {
 public:
-  
+    //TORTOISE and HARE used here
+    // Very Brilliant idea TC O(N) SC O(1)
     bool hasCycle(ListNode *head) {
     ListNode* slow=head;  
     ListNode* fast=head; 
