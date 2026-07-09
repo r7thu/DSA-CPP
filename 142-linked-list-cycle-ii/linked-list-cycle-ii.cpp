19c27eadd0321426edcb12b35b3017ce6ff2c3e1
Rithu <rithuu4@gmail.com>
2026-07-09 09:12:16 +0530

    Time: 7 ms (66.75%) | Memory: 11.3 MB (86.41%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,9 +8,22 @@
  */
 class Solution {
 public:
-    // Brute Force Approach
+    // Optimal Approach
     // Same Solution as in FindCycle
-    // Used Hash Map 
+    // Used Tortoise and Hare
+    // instead of returning true here we find the node where the cycle begins
+    // 
+    // Full Explanation
+    // L1 = distance from start to cycle starting node
+    // D = distance from fast to slow
+    // Length of full cycle L1+D
+    // if distance bw fast and slow is D then d no of iteration is needed
+    // for slow==fast to happen
+    // in d iterations slow will move d steps from the cycle starting node
+    // that means the rest of the distancs is L1
+    // So we can say both distance is L1
+    // So the point of collision will be the starting node of cycle
+    
     ListNode *detectCycle(ListNode *head) {
     ListNode* slow=head;
     ListNode* fast=head;
