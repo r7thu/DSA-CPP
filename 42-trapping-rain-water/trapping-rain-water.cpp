b1cc97b2cc94d7a92a0360419f96cab3a824c045
Rithu <rithuu4@gmail.com>
2026-07-12 12:31:47 +0530

    Time: 3 ms (16.80%) | Memory: 27.4 MB (17.62%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -6,33 +6,37 @@ public:
     // is min[leftMax,rightMax]-height[i]
     // leftMax --> prefixSum
     // rightMax --> suffixSum
+    
     int trap(vector<int>& height) {
         int n=height.size();
         int prefixVal=0;
         vector<int> prefixSum(n);
         int suffixVal=0;
         vector<int> suffixSum(n);
-        prefixSum[0]=height[0];
-        suffixSum[n-1]=height[n-1];
-        for(int i=1;i<n;i++){
-            // if(height[i]>prefixVal)
-            //     prefixVal=height[i];
-            // prefixSum[i]=prefixVal;
-            prefixSum[i]=max(prefixSum[i-1],height[i]);
-        }
-        for(int i=n-2;i>=0;i--){
-            // if(height[i]>suffixVal)
-            //     suffixVal =height[i];
-            // suffixSum[i]=suffixVal;
-            suffixSum[i]=max(suffixSum[i+1],height[i]);
-        }
 
+        // To reduce space complexity we remove this
+        // for(int i=0;i<n;i++){
+        //     if(height[i]>prefixVal)
+        //         prefixVal=height[i];
+        //     prefixSum[i]=prefixVal;
+        // }
+        for(int i=n-1;i>=0;i--){
+            if(height[i]>suffixVal)
+                suffixVal =height[i];
+            suffixSum[i]=suffixVal;
+        }
+        
         int sum=0;
 
         // One thing You need to understand is that
         // the min value of prefixSum[i] or SuffixSum[i] is  height[i]. it cant be less than that so  sum at that point will 0
+
+        int prefixMax=0;
         for(int i=0;i<n;i++){
-                sum+=min(prefixSum[i],suffixSum[i])-height[i];
+                if(height[i]>prefixMax)
+                    prefixMax=height[i];
+
+                sum+=min(prefixMax,suffixSum[i])-height[i];
         }
     return sum;
     }
