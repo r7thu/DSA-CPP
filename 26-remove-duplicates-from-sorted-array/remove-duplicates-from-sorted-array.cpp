08b2c61bcb724261fea73f12f71fe6a5afcf7204
Rithu <rithuu4@gmail.com>
2026-07-12 05:57:59 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (53.30%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     // Brute Force Approach
-    // TC O(NlogN + N)
-    // SC O(N)
+    // TC O(N)
+    // SC O(1)
     int removeDuplicates(vector<int>& nums) {
-        set <int> stt;
         int n = nums.size();
-
-        // ADD element to SET(automatically removes Duplicates)
-        for(int i=0;i<n;i++){
-            stt.insert(nums[i]);
+        int unique=0;
+        for(int j=1;j<n;j++){
+            if(nums[unique]!=nums[j]){
+                unique=unique+1;
+                nums[unique]=nums[j];
+            }
         }
-        int i=0;
-
-        // Reinsert To Array. return last Index of Insertion 
-        for(auto it : stt){
-            nums[i]=it;
-            i++;
-        }
-        return i;
+        return unique+1;
     }
 };
