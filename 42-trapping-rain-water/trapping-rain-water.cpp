76e70b5131bb0ef23d3ca01d8288c2bc91a7624b
Rithu <rithuu4@gmail.com>
2026-07-12 12:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (17.62%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    // Brute Force Solution
+    // Basic Idea
+    // Water trapped at index i
+    // is min[leftMax,rightMax]-height[i]
+    // leftMax --> prefixSum
+    // rightMax --> suffixSum
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int prefixVal=0;
+        vector<int> prefixSum(n);
+        int suffixVal=0;
+        vector<int> suffixSum(n);
+        for(int i=0;i<n;i++){
+            if(height[i]>prefixVal)
+                prefixVal=height[i];
+            prefixSum[i]=prefixVal;
+        }
+        for(int i=n-1;i>=0;i--){
+            if(height[i]>suffixVal)
+                suffixVal =height[i];
+            suffixSum[i]=suffixVal;
+        }
+        
+        int sum=0;
+
+        // One thing You need to understand is that
+        // the min value of prefixSum[i] or SuffixSum[i] is  height[i]. it cant be less than that so  sum at that point will 0
+        for(int i=0;i<n;i++){
+                sum+=min(prefixSum[i],suffixSum[i])-height[i];
+        }
+    return sum;
+    }
+};
