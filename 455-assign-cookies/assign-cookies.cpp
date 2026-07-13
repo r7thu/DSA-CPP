ade161aa94959a522e8f38403bf0af6d96c8c66d
Rithu <rithuu4@gmail.com>
2026-07-13 07:00:07 +0530

    Time: 7 ms (70.65%) | Memory: 44.9 MB (48.18%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+    // Only Approach without recursion/memotization
+    // We compare the starting with the smallest cookie
+    // we move up checking if anyone can eat that
+    // when a child can eat we give it to that child and go to next child
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int n=g.size();
+        int n=g.size(); //Sorting TC O(NlogN)
         int m=s.size();
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
